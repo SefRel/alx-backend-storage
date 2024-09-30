473ca870ad9ea3e80d0d66397e3f83a0b74f14fa
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2024-09-30 22:53:34 +0100

    Add readme file

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- 0. We are all unique!
+-- Comment: Write a SQL script that creates a table users
+CREATE TABLE IF NOT EXISTS users(
+    id INT AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY(id)
+);
